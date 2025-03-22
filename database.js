9b734c69a877e85a2d2f11cfd42a5bff304640b6
elenablissi <163432842+elenablissi@users.noreply.github.com>
2025-03-22 16:47:55 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
 import pg from "pg";
 
-// const db = new pg.Client({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "world",
-//   password: "18mP-z6o",
-//   port: 5432,
-// });
-
 // Creating a new PostgreSQL client using database url
 const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
