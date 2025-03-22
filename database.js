ec073cfe9c72d55f09c8ef98251e9e4135fc54bd
elenablissi <163432842+elenablissi@users.noreply.github.com>
2025-03-22 16:56:59 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,5 @@
 import pg from "pg";
 
-// Importing the 'dotenv' for environment variables management
-import env from "dotenv";
-
 // Creating a new PostgreSQL client using database url
 const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
